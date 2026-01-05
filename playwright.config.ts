71a7e291ed7441068b5186aa621a809a6693e495
atul.anand898 <atul.anand898@gmail.com>
2026-01-05 23:06:02 +0530

    Adding GitHub Actions yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -18,60 +10,110 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],['allure-playwright']
-  ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-   
-    trace: 'on-first-retry',
-    headless:false,
-    screenshot:'on-first-failure',
-    video:'off',
-    baseURL:'https://naveenautomationlabs.com/opencart/index.php'
+  reporter: [
+    ['html'],
+    ['list'],
+    ['allure-playwright'],
+    ['playwright-html-reporter', { 
+      testFolder: 'tests',
+      title: 'OPEN CART HTML Report',
+      project: 'Open Cart',
+      release: '9.87.6',
+      testEnvironment: 'QA',
+      embedAssets: true,
+      embedAttachments: true,
+      outputFolder: 'playwright-html-report',
+      minifyAssets: true,
+      startServer: false,
+    }]
+  ],
   
+  use: {
+    
+    trace: 'on-first-retry',
+    headless: true,
+    screenshot: 'on',
+    video: 'on',
+    baseURL: 'https://naveenautomationlabs.com/opencart/index.php',
+
+    httpCredentials: {
+      username: 'admin',
+      password: 'admin'
+    }
+    
   },
 
   metadata: {
     appUsername: 'pwtest@nal.com',
     appPassword: 'test123'
-    },
+  },
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
+  {
+    name: 'Google Chrome',
+    use: {
+      channel: 'chrome',
+      viewport: null,
+      launchOptions: {
+        args: ['--start-maximized'],
+        ignoreDefaultArgs: ['--window-size=1280,720']
+      }
+    }
+  },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+  // {
+  //   name: 'Microsoft Edge',
+  //   use: {
+  //     channel: 'msedge',
+  //     viewport: null,
+  //     launchOptions: {
+  //       args: ['--start-maximized'],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+  // {
+  //   name: 'Chromium',
+  //   use: {
+  //     browserName: 'chromium',
+  //     viewport: { width: 1920, height: 1080 },
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
 
-   
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-   // },
-    {
-       name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
-  ],
+  // {
+  //   name: 'Firefox',
+  //   use: {
+  //     browserName: 'firefox',
+  //     viewport: { width: 1920, height: 1080 },       
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
 
-  
+  // {
+  //   name: 'WebKit',
+  //   use: {
+  //     browserName: 'webkit',
+  //     viewport: { width: 1920, height: 1080 },      
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // }
+],
+
+
 });
